a3afbed3c37ff587a370f097f22a3b7456ef3dde
Giang Vu Long <longgiang2015bn@gmail.com>
2024-01-07 13:20:23 +0700

    first commit with customized nodes

---
+++ __init__.py
@@ -0,0 +1,74 @@
+import importlib
+import inspect
+import json
+import subprocess
+from pathlib import Path
+
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
+
+config = {
+    "Settings": {
+        "Install Requirements": False,
+        "Update Repository": False,
+        "Quiet Update": True,
+    },
+    "Load Nodes": {
+        "SegToMask": True,
+    },
+}
+
+path = Path(__file__)
+git_path = path.parent
+config_path = path.with_name("config.json")
+req_path = path.with_name("requirements.txt")
+first_run = False
+
+if config_path.is_file():
+    try:
+        with open(config_path, "r") as f:
+            try:
+                dict = json.load(f)
+
+                for key, value in dict.items():
+                    if key in config:
+                        for k, v in value.items():
+                            if k in config[key]:
+                                config[key][k] = v
+            except:
+                print("[\033[94mgiangvlcs\033[0m]: Invalid config. Loading defaults...")
+    except:
+        print("[\033[94mgiangvlcs\033[0m]: Couldn't open config. Loading defaults...")
+else:
+    first_run = True
+
+try:
+    with open(config_path, "w") as f:
+        json.dump(config, f, indent="\t", separators=(",", ": "))
+except:
+    print("[\033[94mgiangvlcs\033[0m]: Couldn't save config. Proceeding...")
+
+quiet = ["-q"] if config["Settings"]["Quiet Update"] else []
+
+if config["Settings"]["Update Repository"]:
+    print("[\033[94mgiangvlcs\033[0m]: Updating repository...")
+    subprocess.run(["git", "-C", git_path, "pull"] + quiet)
+
+if config["Settings"]["Install Requirements"] or first_run:
+    print("[\033[94mgiangvlcs\033[0m]: Installing requirements...")
+    subprocess.run(["python", "-m", "pip", "install", "-U", "-r", req_path] + quiet)
+
+for key, value in config["Load Nodes"].items():
+    if value:
+        module = importlib.import_module(f".nodes.{key}", package=__name__)
+
+        for name, cls in inspect.getmembers(module, inspect.isclass):
+            if cls.__module__ == module.__name__:
+                key = key.replace("_", " ")
+                name = name.replace("_", " ")
+
+                node = f"giangvlcs {key} {name}"
+                disp = f"{key} {name}"
+
+                NODE_CLASS_MAPPINGS[node] = cls
+                NODE_DISPLAY_NAME_MAPPINGS[node] = disp
---
+++ nodes/SegToMask.py
@@ -0,0 +1,211 @@
+import torch
+import numpy as np
+
+from comfy.model_management import InterruptProcessingException, get_torch_device
+from PIL import Image
+from transformers import AutoImageProcessor, UperNetForSemanticSegmentation
+
+LABEL2ID = {
+    "airplane": 90,
+    "animal": 126,
+    "apparel": 92,
+    "arcade machine": 78,
+    "armchair": 30,
+    "ashcan": 138,
+    "awning": 86,
+    "bag": 115,
+    "ball": 119,
+    "bannister": 95,
+    "bar": 77,
+    "barrel": 111,
+    "base": 40,
+    "basket": 112,
+    "bathtub": 37,
+    "bed ": 7,
+    "bench": 69,
+    "bicycle": 127,
+    "blanket": 131,
+    "blind": 63,
+    "boat": 76,
+    "book": 67,
+    "bookcase": 62,
+    "booth": 88,
+    "bottle": 98,
+    "box": 41,
+    "bridge": 61,
+    "buffet": 99,
+    "building": 1,
+    "bulletin board": 144,
+    "bus": 80,
+    "cabinet": 10,
+    "canopy": 106,
+    "car": 20,
+    "case": 55,
+    "ceiling": 5,
+    "chair": 19,
+    "chandelier": 85,
+    "chest of drawers": 44,
+    "clock": 148,
+    "coffee table": 64,
+    "column": 42,
+    "computer": 74,
+    "conveyer belt": 105,
+    "counter": 45,
+    "countertop": 70,
+    "cradle": 117,
+    "crt screen": 141,
+    "curtain": 18,
+    "cushion": 39,
+    "desk": 33,
+    "dirt track": 91,
+    "dishwasher": 129,
+    "door": 14,
+    "earth": 13,
+    "escalator": 96,
+    "fan": 139,
+    "fence": 32,
+    "field": 29,
+    "fireplace": 49,
+    "flag": 149,
+    "floor": 3,
+    "flower": 66,
+    "food": 120,
+    "fountain": 104,
+    "glass": 147,
+    "grandstand": 51,
+    "grass": 9,
+    "hill": 68,
+    "hood": 133,
+    "house": 25,
+    "hovel": 79,
+    "kitchen island": 73,
+    "lake": 128,
+    "lamp": 36,
+    "land": 94,
+    "light": 82,
+    "microwave": 124,
+    "minibike": 116,
+    "mirror": 27,
+    "monitor": 143,
+    "mountain": 16,
+    "ottoman": 97,
+    "oven": 118,
+    "painting": 22,
+    "palm": 72,
+    "path": 52,
+    "person": 12,
+    "pier": 140,
+    "pillow": 57,
+    "plant": 17,
+    "plate": 142,
+    "plaything": 108,
+    "pole": 93,
+    "pool table": 56,
+    "poster": 100,
+    "pot": 125,
+    "radiator": 146,
+    "railing": 38,
+    "refrigerator": 50,
+    "river": 60,
+    "road": 6,
+    "rock": 34,
+    "rug": 28,
+    "runway": 54,
+    "sand": 46,
+    "sconce": 134,
+    "screen": 130,
+    "screen door": 58,
+    "sculpture": 132,
+    "sea": 26,
+    "seat": 31,
+    "shelf": 24,
+    "ship": 103,
+    "shower": 145,
+    "sidewalk": 11,
+    "signboard": 43,
+    "sink": 47,
+    "sky": 2,
+    "skyscraper": 48,
+    "sofa": 23,
+    "stage": 101,
+    "stairs": 53,
+    "stairway": 59,
+    "step": 121,
+    "stool": 110,
+    "stove": 71,
+    "streetlight": 87,
+    "swimming pool": 109,
+    "swivel chair": 75,
+    "table": 15,
+    "tank": 122,
+    "television receiver": 89,
+    "tent": 114,
+    "toilet": 65,
+    "towel": 81,
+    "tower": 84,
+    "trade name": 123,
+    "traffic light": 136,
+    "tray": 137,
+    "tree": 4,
+    "truck": 83,
+    "van": 102,
+    "vase": 135,
+    "wall": 0,
+    "wardrobe": 35,
+    "washer": 107,
+    "water": 21,
+    "waterfall": 113,
+    "windowpane": 8
+  }
+
+class PipelineLoader:
+    @classmethod
+    def INTPUT_TYPES(cls):
+        return {
+            "required": {}
+        }
+    
+    CATEGORY = "giangvlcs/PipelineLoader"
+    FUNCTION = "load"
+    RETURN_NAMES = ("MODEL", "PROCESSOR")
+    RETURN_NAMES = ("MODEL", "MODEL")
+
+    def load(self):
+        image_processor = AutoImageProcessor.from_pretrained("openmmlab/upernet-convnext-small")
+        image_segmentor = UperNetForSemanticSegmentation.from_pretrained("openmmlab/upernet-convnext-small")
+        return image_segmentor, image_processor
+
+class SegToMask:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "classes": ("LIST",), 
+                "image": ("IMAGE",),
+                "model": ("MODEL",), 
+                "processor": ("MODEL",)
+            }
+        }
+        
+    CATEGORY = "giangvlcs/segtomask"
+    TITLE = "Segmentation to Mask"
+    RETURN_TYPES = ("IMAGE",)
+    RUTURN_NAMES = ("masks")
+    FUNCTION = "segment2mask"
+
+    def segment2mask(self, classes, image, model, processor):
+        img = Image.fromarray(image).convert("RGB")
+
+        pixel_values = processor(img, return_tensors="pt").pixel_values
+        with torch.no_grad():
+            outputs = model(pixel_values)
+
+        seg = processor.post_process_semantic_segmentation(outputs, target_sizes=[image.size[::-1]])[0]
+        color_seg = np.zeros((seg.shape[0], seg.shape[1], 3), dtype=np.uint8)
+        list_of_ids = [LABEL2ID[label] for label in classes]
+        for each_id in list_of_ids:
+            color_seg[seg == each_id, :] = np.array([255, 255, 255])
+        color_seg = color_seg.astype(np.uint8)
+        control_image = Image.fromarray(color_seg)
+        return (control_image)
+
